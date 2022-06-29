e82a96808b902baabc94411e653f80a4334c286b
Simon Duc <ducsimon@gmail.com>
2022-06-29 17:27:37 +0200

    add the project progress bar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -64,16 +64,23 @@ export default function Home() {
         <div className="bg-slate-100 rounded-xl p-8 flex flex-col justify-center items-center h-[24rem]">
           <h1 className="text-2xl mb-4">Projet : Chatbot </h1>
           <h2 className="text-xl mb-4">Client : Crédit Agricole</h2>
-          <p className="text-justify text-md">
+          <p className="text-justify text-md mb-4">
             Description : Lorem ipsum dolor sit amet consectetur adipisicing
             elit. Mollitia ducimus eius cumque earum itaque natus, molestias
             sapiente reprehenderit iusto, delectus aliquam nesciunt aut corporis
             exercita.
           </p>
+          <div className="flex flex-col justify-center items-center">
+            Avancement :{" "}
+            <progress id="file" max="100" value="70">
+              {" "}
+              70%{" "}
+            </progress>
+          </div>
         </div>
         <img src={chevronDroit} alt="Chevron droit" className="w-4 ml-2" />
       </section>
-      <section className="">
+      <section>
         <ul className="flex justify-center flex-wrap px-2 mb-2">
           {keywords.map((keyword) => (
             <li
