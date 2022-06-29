cbad68e0d4bb466c9f43a09c331e695a00c1e587
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-06-29 18:54:12 +0200

    Add Link tag to MatchCard.jsx redirecting to MactchCardZoom using params

--- frontend/src/components/MatchCard.jsx
+++ frontend/src/components/MatchCard.jsx
@@ -1,20 +1,23 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-function MatchCard({ match }) {
+function MatchCard({ match, id }) {
   return (
-    <p className="flex flex-col w-[90vw] min-h-[10vh] h-fit border border-black rounded-md justify-center items-center m-4">
-      <h3 className="p-2">{match.name}</h3>
-      <ul className="flex flex-wrap gap-2 p-3">
-        {match.tags.map((tag) => (
-          <li
-            key={tag}
-            className="flex w-fit px-2 justify-center items-center rounded-md bg-orange-300"
-          >
-            {tag}
-          </li>
-        ))}
-      </ul>
-    </p>
+    <Link to={`/match/${id}`}>
+      <p className="flex flex-col w-[90vw] min-h-[10vh] h-fit border border-black rounded-md justify-center items-center m-4">
+        <h3 className="p-2">{match.name}</h3>
+        <ul className="flex flex-wrap gap-2 p-3">
+          {match.tags.map((tag) => (
+            <li
+              key={tag}
+              className="flex w-fit px-2 justify-center items-center rounded-md bg-orange-300"
+            >
+              {tag}
+            </li>
+          ))}
+        </ul>
+      </p>
+    </Link>
   );
 }
 
@@ -22,6 +25,7 @@ MatchCard.propTypes = {
   match: PropTypes.shape.isRequired,
   name: PropTypes.string.isRequired,
   tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+  id: PropTypes.number.isRequired,
 };
 
 export default MatchCard;
