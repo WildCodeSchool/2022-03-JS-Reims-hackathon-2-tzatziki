b6f474fc83a3ad65467af19d330f0da6cf0bfd35
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-06-29 18:55:57 +0200

    Pass id props to MatchCard component in MatchList.jsx

--- frontend/src/components/MatchList.jsx
+++ frontend/src/components/MatchList.jsx
@@ -2,7 +2,9 @@ import likedList from "../dummies/likedList";
 import MatchCard from "./MatchCard";
 
 function MatchList() {
-  return likedList.map((match) => <MatchCard key={match.id} match={match} />);
+  return likedList.map((match) => (
+    <MatchCard key={match.id} match={match} id={match.id} />
+  ));
 }
 
 export default MatchList;
