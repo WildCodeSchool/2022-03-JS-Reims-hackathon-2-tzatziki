dcdfd30270b77e216d33ed193e2ed7bd90b45ff0
loulou0704 <louis51@hotmail.fr>
2022-06-29 16:08:18 +0200

    added style in the nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="flex justify-around text-center text-lg h-[8vh] items-center">
       <NavLink to="/home">🏠</NavLink>
       <NavLink to="/matchlist">❤️</NavLink>
       <NavLink to="/account">👤</NavLink>
