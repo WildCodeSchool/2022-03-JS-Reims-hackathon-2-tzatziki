796f2a22843d7345c02f7d17b346530c20286b5a
JAAFAR CHERIF <JAAFARCHERIF@gmail.com>
2022-06-29 17:52:21 +0200

    added a rule in eslint for label associated control

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,17 +1,76 @@
-import { useUserData } from "../contexts/UserDataContext";
-
 export default function UserProfile() {
-  const { userData } = useUserData();
+  const subsidiary = [
+    { id: 1, name: "Aix-en-Provence" },
+    { id: 2, name: "Aveiro" },
+    { id: 3, name: "Bordeaux" },
+    { id: 4, name: "Brest" },
+    { id: 5, name: "Bruxelles" },
+    { id: 6, name: "Canada" },
+    { id: 7, name: "Casablanca" },
+    { id: 8, name: "Clermont-Ferrand" },
+    { id: 9, name: "Dijon" },
+    { id: 10, name: "Genève" },
+    { id: 11, name: "Le Mans" },
+    { id: 12, name: "Lille" },
+    { id: 13, name: "Lyon" },
+    { id: 14, name: "Montpellier" },
+    { id: 15, name: "Munich" },
+    { id: 16, name: "Nice - Sophia Antipolis" },
+    { id: 17, name: "Nantes" },
+    { id: 18, name: "Niort" },
+    { id: 19, name: "Orléans" },
+    { id: 20, name: "Paris" },
+    { id: 21, name: "Rennes" },
+    { id: 22, name: "Strasbourg" },
+    { id: 23, name: "Toulouse" },
+    { id: 24, name: "Tours" },
+    { id: 25, name: "Vernon" },
+  ];
+
+  function submitForm(event) {
+    event.preventDefault();
+  }
 
   return (
-    <div>
-      <p>Mon compte</p>
-      <ul>
-        <li>Nom : {userData.lastname}</li>
-        <li>Prénom : {userData.firstname}</li>
-        <li>Profession : {userData.profession}</li>
-        <li>Filiale : {userData.subsidiary}</li>
-      </ul>
-    </div>
+    <form
+      className="flex flex-col items-center justify-between"
+      onSubmit={submitForm}
+    >
+      <h2>Mon compte</h2>
+      <label htmlFor="lastname">Nom :</label>
+      <input
+        id="lastname"
+        type="text"
+        name="lastname"
+        className="bg-gray-100"
+      />
+      <label htmlFor="firstname">Prénom :</label>
+      <input
+        id="firstname"
+        type="text"
+        name="firstname"
+        className="bg-gray-100"
+      />
+      <label htmlFor="profession">Profession :</label>
+      <input
+        id="profession"
+        type="text"
+        name="profession"
+        className="bg-gray-100"
+      />
+      <label htmlFor="subsidiary">Filiale :</label>
+      <select id="subsidiary" type="text" name="subsidiary">
+        <option value="">Sélectionnez votre filiale</option>
+        {subsidiary.map((city) => (
+          <option value={city.id}>{city.name}</option>
+        ))}
+      </select>
+      <button
+        type="submit"
+        className=" m-5 p-2 bg-gray-100 border-x-gray-500 border-y-gray-300 border-2 rounded-lg"
+      >
+        Modifier mon profil
+      </button>
+    </form>
   );
 }
