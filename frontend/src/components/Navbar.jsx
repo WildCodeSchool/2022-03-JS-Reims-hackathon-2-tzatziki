08a8eecda2dd36a0d33565920b7751fa514ec0c5
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-06-29 23:48:12 +0200

    Add links to navbar and add matching routes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="flex justify-around w-screen text-center text-lg h-[8vh] items-center bg-slate-100 fixed bottom-0">
-      <NavLink to="/home">🏠</NavLink>
-      <NavLink to="/matchlist">❤️</NavLink>
+      <NavLink to="/">🏠</NavLink>
+      <NavLink to="/match">❤️</NavLink>
       <NavLink to="/account">👤</NavLink>
     </nav>
   );
