c5dfc87c6fd73874e932e2a015ec929e9e230393
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-06-30 20:46:59 +0200

    Create Project page

---
+++ frontend/src/pages/Project.jsx
@@ -0,0 +1,13 @@
+import AddProject from "@components/AddProject";
+import Logo from "@components/Logo";
+
+function Project() {
+  return (
+    <>
+      <Logo />
+      <AddProject />
+    </>
+  );
+}
+
+export default Project;
