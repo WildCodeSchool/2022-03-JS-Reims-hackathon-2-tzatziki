db5f69dbfe206c3068917d6e3405c647dc215f6a
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-06-30 13:57:31 +0200

    Move projects array into dummies

---
+++ frontend/src/dummies/projects.js
@@ -0,0 +1,25 @@
+const projects = [
+  {
+    title: "Chatbot",
+    client: "Crédit Agricole",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora labore, facilis ea suscipit natus repellendus ipsa eos atque, odit, assumenda explicabo recusandae aliquid optio corrupti.",
+    progress: 70,
+  },
+  {
+    title: "Tinderlike",
+    client: "Tinder",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas ea maxime quia! Eligendi at temporibus ex, magni nihil doloremque, fugit magnam odit, voluptatibus ipsa cum",
+    progress: 50,
+  },
+  {
+    title: "Surprise",
+    client: "Tinder",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas ea maxime quia! Eligendi at temporibus ex, magni nihil doloremque, fugit magnam odit, voluptatibus ipsa cum",
+    progress: 50,
+  },
+];
+
+export default projects;
