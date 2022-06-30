ff0700a04e78cb3cd9f1ef9c4f4e4e85a0a7f72d
JAAFAR CHERIF <JAAFARCHERIF@gmail.com>
2022-06-30 09:43:52 +0200

    modified the logo imported

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,3 +1,5 @@
+import logo from "../assets/logo-matchstack-full.png";
+
 export default function Header() {
-  return <img />;
+  return <img src={logo} alt="logo-matchstack" className="h-20" />;
 }
