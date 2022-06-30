a33dbb74ff34c06cd12229cdb07a23fa49abb75a
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-06-30 15:58:11 +0200

    Rework Keywords component

--- frontend/src/components/Keywords.jsx
+++ frontend/src/components/Keywords.jsx
@@ -1,67 +1,22 @@
-const keywords = [
-  {
-    id: 1,
-    name: "React.js",
-  },
-  {
-    id: 2,
-    name: "Javascript",
-  },
-  {
-    id: 3,
-    name: "Express.js",
-  },
-  {
-    id: 4,
-    name: "MySql",
-  },
-  {
-    id: 5,
-    name: "Chatbot",
-  },
-  {
-    id: 6,
-    name: "Chatbot",
-  },
-  {
-    id: 7,
-    name: "Chatbot",
-  },
-  {
-    id: 8,
-    name: "Chatbot",
-  },
-  {
-    id: 9,
-    name: "Chatbot",
-  },
-  {
-    id: 10,
-    name: "Chatbot",
-  },
-  {
-    id: 11,
-    name: "Chatbot",
-  },
-  {
-    id: 12,
-    name: "Chatbot",
-  },
-];
+import PropTypes from "prop-types";
 
-export default function Keywords() {
+function Keywords({ keywords }) {
   return (
-    <section className="mx-8">
-      <ul className="flex justify-center flex-wrap px-2 gap-2">
-        {keywords.map((keyword) => (
-          <li
-            className="flex w-fit px-2 justify-center items-center rounded-md bg-orange-300"
-            key={keyword.id}
-          >
-            {keyword.name}
-          </li>
-        ))}
-      </ul>
-    </section>
+    <ul className="flex flex-wrap p-3 gap-2">
+      {keywords.map((tag) => (
+        <li
+          className="flex w-fit px-2 justify-center items-center rounded-md bg-orange-300"
+          key={tag}
+        >
+          {tag}
+        </li>
+      ))}
+    </ul>
   );
 }
+
+Keywords.propTypes = {
+  keywords: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Keywords;
