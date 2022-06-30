ec09f7d4c155f826da5cb4a19c5fcf4ffe5c37c3
Simon Duc <ducsimon@gmail.com>
2022-06-30 16:42:20 +0200

    add padding in the navbar and increase the size of the 3 navbar logos

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="flex justify-around w-screen text-center text-lg items-center bg-slate-100">
-      <NavLink to="/" className="p-4">
-        🏠
+      <NavLink to="/" className="p-6">
+        <p className="text-2xl">🏠</p>
       </NavLink>
-      <NavLink to="/match" className="p-4">
-        ❤️
+      <NavLink to="/match" className="p-6">
+        <p className="text-2xl">❤️</p>
       </NavLink>
-      <NavLink to="/account" className="p-4">
-        👤
+      <NavLink to="/account" className="p-6">
+        <p className="text-2xl">👤</p>
       </NavLink>
     </nav>
   );
