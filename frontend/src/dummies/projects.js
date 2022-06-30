f84a39af2026cc2919c06e105a4bf871bb39269b
loulou0704 <louis51@hotmail.fr>
2022-06-30 17:34:41 +0200

    added projects in the project.js file

--- frontend/src/dummies/projects.js
+++ frontend/src/dummies/projects.js
@@ -32,6 +32,84 @@ const projects = [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora labore, facilis ea suscipit natus repellendus ipsa eos atque, odit, assumenda explicabo recusandae aliquid optio corrupti.",
     progress: 50,
   },
+  {
+    title: "SearchEngine",
+    client: "Google",
+    subsidiary: "Paris",
+    staff: ["John Doe", "Jane Doe", "Jack Doe", "Jill Doe", "Jenny Doe"],
+    keyword: ["PHP", "React", "Node", "styled-components", "tailwindcss"],
+    slackURL: "https://gloubiboulga.slack.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora labore, facilis ea suscipit natus repellendus ipsa eos atque, odit, assumenda explicabo recusandae aliquid optio corrupti.",
+    progress: 20,
+  },
+  {
+    title: "Maj iOS",
+    client: "Apple",
+    subsidiary: "Paris",
+    staff: ["John Doe", "Jane Doe", "Jack Doe", "Jill Doe", "Jenny Doe"],
+    keyword: ["PHP", "React", "Node", "styled-components", "tailwindcss"],
+    slackURL: "https://gloubiboulga.slack.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora labore, facilis ea suscipit natus repellendus ipsa eos atque, odit, assumenda explicabo recusandae aliquid optio corrupti.",
+    progress: 40,
+  },
+
+  {
+    title: "Logiciel",
+    client: "IBM",
+    subsidiary: "Lyon",
+    staff: ["John Doe", "Jane Doe", "Jack Doe", "Jill Doe", "Jenny Doe"],
+    keyword: ["PHP", "React", "Node", "styled-components", "tailwindcss"],
+    slackURL: "https://gloubiboulga.slack.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora labore, facilis ea suscipit natus repellendus ipsa eos atque, odit, assumenda explicabo recusandae aliquid optio corrupti.",
+    progress: 60,
+  },
+  {
+    title: "MarketPlace",
+    client: "Facebbok",
+    subsidiary: "Paris",
+    staff: ["John Doe", "Jane Doe", "Jack Doe", "Jill Doe", "Jenny Doe"],
+    keyword: ["PHP", "React", "Node", "styled-components", "tailwindcss"],
+    slackURL: "https://gloubiboulga.slack.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora labore, facilis ea suscipit natus repellendus ipsa eos atque, odit, assumenda explicabo recusandae aliquid optio corrupti.",
+    progress: 30,
+  },
+  {
+    title: "Logiciel",
+    client: "SAP",
+    subsidiary: "Bordeaux",
+    staff: ["John Doe", "Jane Doe", "Jack Doe", "Jill Doe", "Jenny Doe"],
+    keyword: ["PHP", "React", "Node", "styled-components", "tailwindcss"],
+    slackURL: "https://gloubiboulga.slack.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora labore, facilis ea suscipit natus repellendus ipsa eos atque, odit, assumenda explicabo recusandae aliquid optio corrupti.",
+    progress: 70,
+  },
+  {
+    title: "Store",
+    client: "Microsoft",
+    subsidiary: "Lille",
+    staff: ["John Doe", "Jane Doe", "Jack Doe", "Jill Doe", "Jenny Doe"],
+    keyword: ["PHP", "React", "Node", "styled-components", "tailwindcss"],
+    slackURL: "https://gloubiboulga.slack.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora labore, facilis ea suscipit natus repellendus ipsa eos atque, odit, assumenda explicabo recusandae aliquid optio corrupti.",
+    progress: 50,
+  },
+  {
+    title: "Cloud",
+    client: "TENCENT",
+    subsidiary: "Bordeaux",
+    staff: ["John Doe", "Jane Doe", "Jack Doe", "Jill Doe", "Jenny Doe"],
+    keyword: ["PHP", "React", "Node", "styled-components", "tailwindcss"],
+    slackURL: "https://gloubiboulga.slack.com",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora labore, facilis ea suscipit natus repellendus ipsa eos atque, odit, assumenda explicabo recusandae aliquid optio corrupti.",
+    progress: 50,
+  },
 ];
 
 export default projects;
