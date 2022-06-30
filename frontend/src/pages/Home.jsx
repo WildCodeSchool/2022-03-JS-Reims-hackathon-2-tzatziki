3b21402ede7e3dddcdace26ae2a1d1f1d8647a08
JAAFAR CHERIF <JAAFARCHERIF@gmail.com>
2022-06-30 13:58:33 +0200

    changed the background color, margin and border of the toast

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import TinderCard from "react-tinder-card";
 import Header from "@components/Header";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "../App.css";
 
 const keywords = [
   {
@@ -68,17 +71,28 @@ const projects = [
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas ea maxime quia! Eligendi at temporibus ex, magni nihil doloremque, fugit magnam odit, voluptatibus ipsa cum",
     progress: 50,
   },
+  {
+    title: "TinderSurprise",
+    client: "Tinder",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas ea maxime quia! Eligendi at temporibus ex, magni nihil doloremque, fugit magnam odit, voluptatibus ipsa cum",
+    progress: 50,
+  },
 ];
 
 const matches = [];
+const toastStyle = {
+  success: "bg-orange-300 text-white font-bold m-2 rounded-lg",
+};
 
 export default function Home() {
-  const [lastDirection, setLastDirection] = useState();
+  const setLastDirection = useState()[1];
 
   const swiped = (direction, title, client, description, progress) => {
     setLastDirection(direction);
     if (direction === "right") {
       matches.push({ title, client, description, progress });
+      toast.success("Matché ❤️ !");
     }
     console.warn(matches);
   };
@@ -122,9 +136,6 @@ export default function Home() {
           ))}
         </div>
       </section>
-      <section className="text-center">
-        {lastDirection === "right" ? <p>Matché ❤️ !</p> : <p />}
-      </section>
       <section>
         <ul className="flex justify-center flex-wrap px-2 mb-2">
           {keywords.map((keyword) => (
@@ -138,6 +149,18 @@ export default function Home() {
         </ul>
       </section>
       <div />
+      <ToastContainer
+        toastClassName={`${toastStyle.success}`}
+        position="top-center"
+        autoClose={10000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </main>
   );
 }
