48d8b8549f429f8328a9e90d416e25ce32dfb17f
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-06-30 16:00:41 +0200

    Remove Keywords component from Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,14 +2,12 @@ import { ToastContainer } from "react-toastify";
 import Logo from "@components/Logo";
 import "react-toastify/dist/ReactToastify.css";
 import TinderCard from "@components/TinderCard";
-import Keywords from "@components/Keywords";
 
 export default function Home() {
   return (
     <div className="h-full flex flex-col place-content-around">
       <Logo />
       <TinderCard />
-      <Keywords />
       <ToastContainer
         position="top-center"
         autoClose={1500}
